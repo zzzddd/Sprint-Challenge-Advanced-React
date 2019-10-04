c536e0b0e084a05fc3875cfa8bd78eb05b3ff01f
zee <zzdd9251@gmail.com>
2019-10-04 09:14:35 -0700

    inital..

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+
+class App extends React.Component {
+  state = {
+    data: "",
+    user: "zzzddd",
+    followers: []
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/players')
+
+      .then(res => {
+        console.log("players data...", res);
+        this.setState({ data: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    console.log("Followers", this.state.followers);
+    return (
+      <div>
+       dataheare
+      </div>
+    );
+  }
 }
 
 export default App;
